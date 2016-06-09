2ba4422411081c0f87dc96a32a7f5795230e7c88
Pan303 <darkamazone303@gmail.com>
2016-06-09 12:01:07 +0200

    un message

---
+++ accueil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="css.css"/>
+
+
+    <title>Site Client : Accueil</title>
+
+</head>
+<body>
+<?php
+include ("menu.php");
+?>
+
+<article>
+         <a href="conseillere_en_image.php" class="classname">Conseillère en image</a>
+        <a href="creation.php" class="classname">Créations : bijoux & accessoires</a>
+         <a href="peinture.php" class="classname">Peintures</a>
+</article>
+
+
+<article>
+    <h1>Présentation de Sophie</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus tortor augue, sed varius ante ornare eget. Morbi imperdiet, justo id placerat consectetur, nunc tellus ullamcorper turpis, eu accumsan enim erat et mi. Quisque sit amet tortor non nunc finibus ultrices. Phasellus et ante libero. Nunc egestas quis turpis eget vestibulum. Proin vitae dictum enim. Mauris ornare, lorem imperdiet semper auctor, ex sem tristique odio, vitae dapibus felis massa ut tortor. Vestibulum a dolor mauris. Cras ornare in odio ut vehicula. Aliquam tincidunt risus est, nec lobortis felis consectetur quis. Integer eget neque ac tortor rutrum lacinia. Mauris ullamcorper nunc id ultricies accumsan. Aliquam tincidunt magna turpis, vel maximus neque maximus commodo. Proin eget condimentum felis. Pellentesque dictum libero non nisl vestibulum fermentum. Cras tempus sollicitudin nisi, vel mollis est facilisis ac.
+
+    </p>
+</article>
+
+<footer>
+
+</footer>
+
+
+
