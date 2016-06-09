42ee99a7bb2e9e1b8b6ec313624ca41aa209ab24
miloon <medellyna@gmail.com>
2016-06-09 11:09:05 +0200

    ajout config et connexion

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+try {
+    $hostname = 'localhost';
+    $user = 'root';
+    $pass = 'root'; // sous windows mettre '';
+    $dbh = new PDO("mysql:host=$hostname;dbname=sophie", $user, $pass);
+    $dbh->exec("SET CHARACTER SET utf8");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
