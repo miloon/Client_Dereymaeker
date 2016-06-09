96196fe8146fa49f5911a18f65a905809b37bab1
Dalyome <dalyo1190@hotmail.be>
2016-06-09 11:38:48 +0200

    modif config db

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ session_start();
 
 //recu de connexion db
 require_once "config.php";
-require_once "modele/db.php";
+
 
 //verification si connecter ou non
 
 if (!isset($_SESSION["id"])|| $_SESSION["id"]!= session_id()) {
-    require_once "controleur/routuer.php";
+    require_once "controleur/routeur.php";
 }else{
     require_once "controleur/routeuradmin.php";
 }
