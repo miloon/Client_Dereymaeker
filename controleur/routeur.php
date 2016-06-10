d43474deb89c8e8164e32618564a2695c8bf5835
Pan303 <darkamazone303@gmail.com>
2016-06-10 09:49:14 +0200

    modif routeur

--- controleur/routeur.php
+++ controleur/routeur.php
@@ -2,35 +2,48 @@
 <?php
 
 if(isset($_GET['deco'])){
-    require_once "deco.php";
+    require_once "modele/deco.php";
 
 }elseif($_GET['connect']){
-    require_once "connect";
+    require_once "modele/connect";
+    require_once "vue/connect";
+
 }
 
 
 
 elseif(!isset($_GET['section'])) {
-    include 'accueil.php';
+    include 'modele/accueil.php';
+    include 'vue/accueil.php';
 }else{
     $section = $_GET['section'];
     switch($section){
         case "conseillere_en_image":
-            include 'conseillere_en_image.php';
+            include 'modele/conseillere_en_image.php';
+            include 'vue/conseillere_en_image.php';
+
             break;
         case "creation":
-            include 'creation.php';
+            include 'modele/creation.php';
+            include 'vue/creation.php';
+
             break;
         case "peinture":
-            include 'peinture.php';
+            include 'modele/peinture.php';
+            include 'vue/peinture.php';
+
             break;
         case "contact":
-            include 'contact.php';
+            include 'modele/contact.php';
+            include 'vue/contact.php';
+
             break;
 
 
         default :
-            include 'accueil.php';
+            include 'modele/accueil.php';
+            include 'vue/accueil.php';
+
     }
 
 }
