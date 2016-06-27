b1f1031c8976a0dab122b0b9a7968466e5bbd432
miloon <medellyna@gmail.com>
2016-06-27 16:09:29 +0200

    css et modif accueil

--- vue/accueil.php
+++ vue/accueil.php
@@ -10,62 +10,7 @@
     <link href="vue/css/bootstrap.css" rel="stylesheet">
     <link href="vue/css/bootstrap-responsive.css" rel="stylesheet">
     <link href="vue/css/style.css" rel="stylesheet">
-    <style>
 
-        .classname {
-            -moz-box-shadow: inset 0px 0px 12px 14px #ffffff;
-            -webkit-box-shadow: inset 0px 0px 12px 14px #ffffff;
-            box-shadow: inset 0px 0px 12px 14px #ffffff;
-            background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ededed), color-stop(1, #dfdfdf));
-            background: -moz-linear-gradient(center top, #ededed 5%, #dfdfdf 100%);
-            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ededed', endColorstr='#dfdfdf');
-            background-color: #ededed;
-            -webkit-border-top-left-radius: 0px;
-            -moz-border-radius-topleft: 0px;
-            border-top-left-radius: 0px;
-            -webkit-border-top-right-radius: 0px;
-            -moz-border-radius-topright: 0px;
-            border-top-right-radius: 0px;
-            -webkit-border-bottom-right-radius: 0px;
-            -moz-border-radius-bottomright: 0px;
-            border-bottom-right-radius: 0px;
-            -webkit-border-bottom-left-radius: 0px;
-            -moz-border-radius-bottomleft: 0px;
-            border-bottom-left-radius: 0px;
-            text-indent: 0px;
-            border: 5px solid #dcdcdc;
-            display: inline-block;
-            color: #777777;
-            font-family: Arial;
-            font-size: 15px;
-            font-weight: bold;
-            font-style: normal;
-            height: 200px;
-            line-height: 200px;
-            width: 250px;
-            text-decoration: none;
-            text-align: center;
-            text-shadow: 0px 1px 0px #ffffff;
-        }
-
-        .classname:hover {
-            background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #dfdfdf), color-stop(1, #ededed));
-            background: -moz-linear-gradient(center top, #dfdfdf 5%, #ededed 100%);
-            filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#dfdfdf', endColorstr='#ededed');
-            background-color: #dfdfdf;
-        }
-
-        .classname:active {
-            position: relative;
-            top: 1px;
-        }
-
-        article {
-            margin-top: 100px;
-            text-align: center;
-        }
-
-    </style>
     <!--Font-->
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet'
           type='text/css'>
@@ -91,15 +36,39 @@
 <!--HEADER ROW-->
 <?php include 'vue/menu.php' ?>
 <!-- /HEADER ROW -->
-
-<article>
-    <a href="?section=conseil" class="classname">Conseillère en image</a>
-    <a href="?section=creation" class="classname">Hand made craft</a>
-    <a href="?section=peinture" class="classname">Artiste peintre</a>
-</article>
-
-
 <div class="container">
+    <div class="row">
+        <div class="span12">
+            <ul id="accueil" class="thumbnails">
+                <li class="span4">
+                    <div class="thumbnail">
+                        <a href="?section=conseil"><img alt="" src="vue/img/presentation/conseil.jpg"/></a>
+                        <div class="caption">
+                            <strong>Conseillère en image</strong>
+                        </div>
+                    </div>
+                </li>
+                <li class="span4">
+                    <div class="thumbnail">
+                        <a href="?section=creation"><img alt="300x200" src="vue/img/presentation/handmadecraft.jpg"/></a>
+                        <div class="caption">
+                            <strong>Hand Made Craft</strong>
+                        </div>
+                    </div>
+                </li>
+                <li class="span4">
+                    <div class="thumbnail">
+                        <a href="?section=peinture"><img alt="300x200" src="vue/img/presentation/peinture.jpg"/></a>
+                        <div class="caption">
+                            <strong>Artiste peintre</strong>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
     <div class="row">
         <div class="span12">
             <div class="page-header">
@@ -112,19 +81,34 @@
     <div class="container">
         <div class="row">
 
-            <div class="span12"><p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus tortor augue, sed varius ante
-                    ornare eget. Morbi imperdiet, justo id placerat consectetur, nunc tellus ullamcorper turpis, eu
-                    accumsan enim erat et mi. Quisque sit amet tortor non nunc finibus ultrices. Phasellus et ante
-                    libero. Nunc egestas quis turpis eget vestibulum. Proin vitae dictum enim. Mauris ornare, lorem
-                    imperdiet semper auctor, ex sem tristique odio, vitae dapibus felis massa ut tortor. Vestibulum a
-                    dolor mauris. Cras ornare in odio ut vehicula. Aliquam tincidunt risus est, nec lobortis felis
-                    consectetur quis. Integer eget neque ac tortor rutrum lacinia. Mauris ullamcorper nunc id ultricies
-                    accumsan. Aliquam tincidunt magna turpis, vel maximus neque maximus commodo. Proin eget condimentum
-                    felis. Pellentesque dictum libero non nisl vestibulum fermentum. Cras tempus sollicitudin nisi, vel
-                    mollis est facilisis ac.
+            <div class="span12">
+                <div class="media">
+                    <img class="img-responsive pull-left media-object" src="vue/img/presentation/sophie1.jpg"/>
+                    <div class="media-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus tortor augue, sed varius
+                            ante
+                            ornare eget. Morbi imperdiet, justo id placerat consectetur, nunc tellus ullamcorper turpis,
+                            eu
+                            accumsan enim erat et mi. Quisque sit amet tortor non nunc finibus ultrices. Phasellus et
+                            ante
+                            libero. Nunc egestas quis turpis eget vestibulum. Proin vitae dictum enim. Mauris ornare,
+                            lorem
+                            imperdiet semper auctor, ex sem tristique odio, vitae dapibus felis massa ut tortor.
+                            Vestibulum a
+                            dolor mauris. Cras ornare in odio ut vehicula. Aliquam tincidunt risus est, nec lobortis
+                            felis
+                            consectetur quis. Integer eget neque ac tortor rutrum lacinia. Mauris ullamcorper nunc id
+                            ultricies
+                            accumsan. Aliquam tincidunt magna turpis, vel maximus neque maximus commodo. Proin eget
+                            condimentum
+                            felis. Pellentesque dictum libero non nisl vestibulum fermentum. Cras tempus sollicitudin
+                            nisi, vel
+                            mollis est facilisis ac.
 
-                </p></div>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- vue/conseil.php
+++ vue/conseil.php
@@ -1,86 +1,86 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Sophie Dereymaeker</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<!-- Bootstrap -->
-	<link href="vue/css/bootstrap.css" rel="stylesheet">
-	<link href="vue/css/bootstrap-responsive.css" rel="stylesheet">
-	<link href="vue/css/style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>Sophie Dereymaeker</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Bootstrap -->
+    <link href="vue/css/bootstrap.css" rel="stylesheet">
+    <link href="vue/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="vue/css/style.css" rel="stylesheet">
 
-	<!--Font-->
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet'
-		  type='text/css'>
-
-
-	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-
-	<!-- Fav and touch icons -->
-	<link rel="shortcut icon" href="ico/favicon.ico">
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-
-	<style>
-		/* http://jsfiddle.net/ivanarvizu/JS6JV/16/ */
-		#carousel-custom {
-			margin: 20px auto;
-			width: 80%;
-		}
-
-		#carousel-custom .carousel-indicators {
-			margin: 10px 0 0;
-			overflow: auto;
-			position: static;
-			text-align: left;
-			white-space: nowrap;
-			width: 100%;
-		}
-
-		#carousel-custom .carousel-indicators li {
-			background-color: transparent;
-			-webkit-border-radius: 0;
-			border-radius: 0;
-			display: inline-block;
-			height: auto;
-			margin: 0 !important;
-			width: auto;
-		}
-
-		#carousel-custom .carousel-indicators li img {
-			display: block;
-			opacity: 0.5;
-			height: 90px;
-		}
-
-		#carousel-custom .carousel-indicators li.active img {
-			opacity: 1;
-		}
-
-		#carousel-custom .carousel-indicators li:hover img {
-			opacity: 0.75;
-		}
-
-		#carousel-custom .carousel-outer {
-			position: relative;
-		}
-	</style>
+    <!--Font-->
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet'
+          type='text/css'>
 
 
-	<!-- SCRIPT
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+
+    <style>
+        /* http://jsfiddle.net/ivanarvizu/JS6JV/16/ */
+        #carousel-custom {
+            margin: 20px auto;
+            width: 80%;
+        }
+
+        #carousel-custom .carousel-indicators {
+            margin: 10px 0 0;
+            overflow: auto;
+            position: static;
+            text-align: left;
+            white-space: nowrap;
+            width: 100%;
+        }
+
+        #carousel-custom .carousel-indicators li {
+            background-color: transparent;
+            -webkit-border-radius: 0;
+            border-radius: 0;
+            display: inline-block;
+            height: auto;
+            margin: 0 !important;
+            width: auto;
+        }
+
+        #carousel-custom .carousel-indicators li img {
+            display: block;
+            opacity: 0.5;
+            height: 90px;
+        }
+
+        #carousel-custom .carousel-indicators li.active img {
+            opacity: 1;
+        }
+
+        #carousel-custom .carousel-indicators li:hover img {
+            opacity: 0.75;
+        }
+
+        #carousel-custom .carousel-outer {
+            position: relative;
+        }
+    </style>
+
+
+    <!-- SCRIPT
     ============================================================-->
-	<script src="http://code.jquery.com/jquery.js"></script>
-	<script src="vue/js/bootstrap.min.js"></script>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="vue/js/bootstrap.min.js"></script>
 
     <!-- SCRIPT 
-    ============================================================-->  
+    ============================================================-->
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="../modele/js/bootstrap.min.js"></script>
 </head>
@@ -88,150 +88,144 @@
 
 <!--HEADER ROW-->
 <div id="header-row">
-	<div class="container">
-		<div class="row">
-			<!-- MAIN NAVIGATION -->
-			<div class="span12">
-				<div class="navbar">
-					<div class="navbar-inner">
-						<?php include 'vue/menu.php' ?>
-					</div>
-				</div>
-			</div>
-			<!-- MAIN NAVIGATION -->
+    <div class="container">
+        <div class="row">
+            <!-- MAIN NAVIGATION -->
+            <div class="span12">
+                <div class="navbar">
+                    <div class="navbar-inner">
+                        <?php include 'vue/menu.php' ?>
+                    </div>
+                </div>
+            </div>
+            <!-- MAIN NAVIGATION -->
 
 
+            <div class="container">
+                <!--PAGE TITLE-->
+
+                <div class="row">
+                    <div class="span12">
+                        <div class="page-header">
+                            <h1>
+                                Services
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /HEADER ROW -->
 
 
-<div class="container">
-	  <!--PAGE TITLE-->
+            <div class="container">
+                <div class="row">
+                    <div class="span12">
+                        <div class="page-header">
+                            <h1>Créations & Customisations</h1>
+                        </div>
+                        <div id="references">
+                            <h3>uis iaculis pharetra. Nullam facilisis urna ac magna iacul</h3>
 
-	<div class="row">
-		<div class="span12">
-		<div class="page-header">
-				<h1>
-				Services
-			</h1>
-		</div>
-		</div>
-	</div>
-</div>
-<!-- /HEADER ROW -->
+                            <ul>
+                                <li><a id="colorimétrie" href="javascript:;" onclick="a();">Colorimétrie</a></li>
+                                <li><a id="analyse" href="javascript:;" onclick="b();">Analyse</a></li>
+                                <li><a id="tri" href="javascript:;" onclick="c();">Tri</a></li>
+                                <li><a id="accompagnement" href="javascript:;" onclick="d();">Accompagnement</a></li>
+                                <li><a id="cours" href="javascript:;" onclick="e();">Cours</a></li>
+                                <li><a id="decoration" href="javascript:;" onclick="a();">décoration</a></li>
+                            </ul>
 
-
-<div class="container">
-	<div class="row">
-		<div class="span12">
-			<div class="page-header">
-				<h1>Créations & Customisations</h1>
-			</div>
-			<div id="references">
-				<h3>uis iaculis pharetra. Nullam facilisis urna ac magna iacul</h3>
-
-				<ul>
-					<li><a id="colorimétrie" href="javascript:;" onclick="a();">Colorimétrie</a></li>
-					<li><a id="analyse" href="javascript:;" onclick="b();">Analyse</a></li>
-					<li><a id="tri" href="javascript:;" onclick="c();">Tri</a></li>
-					<li><a id="accompagnement" href="javascript:;" onclick="d();">Accompagnement</a></li>
-					<li><a id="cours" href="javascript:;" onclick="e();">Cours</a></li>
-					<li><a id="decoration" href="javascript:;" onclick="a();">décoration</a></li>
-				</ul>
-
-			</div>
-			<div id="corps">
-				<div id="menu">
-				</div>
+                        </div>
+                        <div id="corps">
+                            <div id="menu">
+                            </div>
 
 
-				<script>
+                            <script>
 
-					function a()
-					{
-						document.getElementById('menu').innerHTML = 'Nam quis varius est, a tincidunt nulla. In eget ex est. Cras dignissim maximus tincidunt. Class aptent taciti sociosqu ad."dapibus consectetur. Proin finibus orci ante, at tincidunt massa convallis sed". Proin consectetur sem quis iaculis pharetra. Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibus ante';
-					}
-					function b()
-					{
-						document.getElementById('menu').innerHTML = 'Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibus ante';
-					}
-					function c()
-					{
-						document.getElementById('menu').innerHTML = 'onsectetur sem quis iaculis pharetra. Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibu';
-					}
-					function d()
-					{
-						document.getElementById('menu').innerHTML = 's dapibus consectetur. Proin finibus orci ante, at tincidunt massa convallis sed. Proin consectetur sem quis iaculis pharetra. Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibus anilisis urna ac magna';
-					}
-					function e()
-					{
-						document.getElementById('menu').innerHTML = 'Venas ac diam non risus malesua';
-					}
+                                function a() {
+                                    document.getElementById('menu').innerHTML = 'Nam quis varius est, a tincidunt nulla. In eget ex est. Cras dignissim maximus tincidunt. Class aptent taciti sociosqu ad."dapibus consectetur. Proin finibus orci ante, at tincidunt massa convallis sed". Proin consectetur sem quis iaculis pharetra. Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibus ante';
+                                }
+                                function b() {
+                                    document.getElementById('menu').innerHTML = 'Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibus ante';
+                                }
+                                function c() {
+                                    document.getElementById('menu').innerHTML = 'onsectetur sem quis iaculis pharetra. Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibu';
+                                }
+                                function d() {
+                                    document.getElementById('menu').innerHTML = 's dapibus consectetur. Proin finibus orci ante, at tincidunt massa convallis sed. Proin consectetur sem quis iaculis pharetra. Nullam facilisis urna ac magna iaculis dapibus. Maecenas ac diam non risus malesuada pharetra. Praesent finibus anilisis urna ac magna';
+                                }
+                                function e() {
+                                    document.getElementById('menu').innerHTML = 'Venas ac diam non risus malesua';
+                                }
 
-					window.onload = function()
-					{
-						a();
-					};
+                                window.onload = function () {
+                                    a();
+                                };
 
-				</script>
+                            </script>
 
 
-			</div>
-		</div>
-	</div>
-	
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div id='carousel-custom' class='carousel slide' data-ride='carousel'>
-				<div class='carousel-outer'>
-					<!-- Wrapper for slides -->
-					<div class='carousel-inner'>
+                        </div>
+                    </div>
+                </div>
 
-						<?php
-						$i = 1;
-						foreach ($affiche_temoignage as $recbig) {
-							?>
-							<?php if ($i == 1): ?>
-								<div class='item active'>
-									<h3><?= $recbig->nom ?></h3>
-									<p><?= $recbig->texte ?></p>
-									<h3><? $recbig->email ?></h3>
-									<p><?= $recbig->profession ?></p>
-									<p><?= $recbig->age ?></p>
-								</div>
-							<?php else: ?>
-								<div class='item'>
-									<h3><?= $recbig->nom ?></h3>
-									<p><?= $recbig->texte ?></p>
-									<h3><? $recbig->email ?></h3>
-									<p><?= $recbig->profession ?></p>
-									<p><?= $recbig->age ?></p>
-								</div>
-							<?php endif; ?>
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                        <div id='carousel-custom' class='carousel slide' data-ride='carousel'>
+                            <div class='carousel-outer'>
+                                <!-- Wrapper for slides -->
+                                <div class='carousel-inner'>
 
-							<?php
-							$i++;
-						}
-						?>
+                                    <?php
+                                    $i = 1;
+                                    foreach ($affiche_temoignage as $recbig) {
+                                        ?>
+                                        <?php if ($i == 1): ?>
+                                            <div class='item active'>
+                                                <h3><?= $recbig->nom ?></h3>
+                                                <p><?= $recbig->texte ?></p>
+                                                <h3><? $recbig->email ?></h3>
+                                                <p><?= $recbig->profession ?></p>
+                                                <p><?= $recbig->age ?></p>
+                                            </div>
+                                        <?php else: ?>
+                                            <div class='item'>
+                                                <h3><?= $recbig->nom ?></h3>
+                                                <p><?= $recbig->texte ?></p>
+                                                <h3><? $recbig->email ?></h3>
+                                                <p><?= $recbig->profession ?></p>
+                                                <p><?= $recbig->age ?></p>
+                                            </div>
+                                        <?php endif; ?>
 
-					</div>
+                                        <?php
+                                        $i++;
+                                    }
+                                    ?>
 
-					<!-- Controls -->
-					<a class='left carousel-control' href='#carousel-custom' data-slide='prev'>
-						<span class='glyphicon glyphicon-chevron-left'></span>
-					</a>
-					<a class='right carousel-control' href='#carousel-custom' data-slide='next'>
-						<span class='glyphicon glyphicon-chevron-right'></span>
-					</a>
-				</div>
+                                </div>
 
-				<!-- Indicators -->
-				
-					<p style="text-align: center;">
-						<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script></p>
+                                <!-- Controls -->
+                                <a class='left carousel-control' href='#carousel-custom' data-slide='prev'>
+                                    <span class='glyphicon glyphicon-chevron-left'></span>
+                                </a>
+                                <a class='right carousel-control' href='#carousel-custom' data-slide='next'>
+                                    <span class='glyphicon glyphicon-chevron-right'></span>
+                                </a>
+                            </div>
 
-				</ol>
-			</div>
-		</div>
-	</div>
+                            <!-- Indicators -->
+
+                            <p style="text-align: center;">
+                                <script type="text/javascript"
+                                        src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+                            </p>
+
+                            </ol>
+                        </div>
+                    </div>
+                </div>
 
 
 
