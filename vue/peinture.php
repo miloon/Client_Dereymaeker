4812ca8d5e0be22d6a5e2999e1378e9d34d4cb2c
Dalyome <dalyo1190@hotmail.be>
2016-06-28 14:03:45 +0200

    mise en place include

--- vue/peinture.php
+++ vue/peinture.php
@@ -1,40 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Sophie Dereymaeker</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- Bootstrap -->
-    <link href="vue/css/bootstrap.css" rel="stylesheet">
-    <link href="vue/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="vue/css/style.css" rel="stylesheet">
-
-    <!--Font-->
-    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600' rel='stylesheet'
-          type='text/css'>
-
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
-    <link rel="shortcut icon" href="ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-
-
-    <!-- SCRIPT
-    ============================================================-->
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="vue/js/bootstrap.min.js"></script>
-
-</head>
+<?php
+include_once "header.php"
+?>
 <body>
 <!--HEADER ROW-->
                         <?php include 'vue/menu.php' ?>
@@ -147,23 +113,9 @@
     <!--Footer
     ==========================-->
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="span6"> Sophie Dereymaeker <br/>
-                    <small> Créatrice de styles</small>
-                </div>
-                <div class="span6">
-                    <div class="social pull-right">
-                        <a href="#"><img src="vue/img/social/dribbble.png" alt=""></a>
-                        <a href="#"><img src="vue/img/social/twitter.png" alt=""></a>
-                        <a href="#"><img src="vue/img/social/dribbble.png" alt=""></a>
-                        <a href="#"><img src="vue/img/social/rss.png" alt=""></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php
+    include_once "footer.php"
+    ?>
 
     <!--/.Footer-->
 
